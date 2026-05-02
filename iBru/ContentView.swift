2bc118bc37bf43a662491ac6f78cdf29b8b4f2a0
Gerard Cañadas <gerard.canadas@gmail.com>
2026-05-02 21:42:02 +0200

    Initial Commit

---
+++ iBru/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iBru
+//
+//  Created by Gerard Cañadas on 02/05/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iBru/iBruApp.swift
@@ -0,0 +1,17 @@
+//
+//  iBruApp.swift
+//  iBru
+//
+//  Created by Gerard Cañadas on 02/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct iBruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
